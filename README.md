aee6ca9b53cd976cca1074143a6e0f0ecf227861
brian <koreanderson@gmail.com>
2017-06-21 14:55:23 -0400

    add carousel with filter slides

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # slider-transitions
+
+A quick repo made to play around with css filters and to be used as a reference for future projects.
---
+++ functions.js
@@ -0,0 +1,7 @@
+var slider = new Swiper('.swiper-container', {
+	direction: 'horizontal',
+    loop: true,
+	nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+});
+
